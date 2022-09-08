f39b476f3388cc8d38758dbeba61bc202c61568c
mohdjunaid0741 <109589436+mohdjunaid0741@users.noreply.github.com>
2022-09-08 21:19:36 +0530

    Add files via upload

---
+++ Assignment-4(Questin-1).java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Q1 {
+public static void main(String[]args) {
+	HashMap<Integer,Integer> hm = new HashMap<>();
+	
+	hm.put(1,1);
+	hm.put(2,4);
+	hm.put(3,9);
+	hm.put(4,16);
+	hm.put(5,25);
+	hm.put(6,36);
+	hm.put(7,49);
+	hm.put(8,64);
+	hm.put(9,81);
+	hm.put(10,100);
+	hm.put(11,121);
+	hm.put(12,144);
+	hm.put(13,169);
+	hm.put(14,196);
+	hm.put(15,225);
+	
+	System.out.println(hm);
+}
+}
